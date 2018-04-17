600482f21c49d0172ffb3c3e6e455e3afd60cd08
Nil Késede <nil@ksde.pw>
2018-04-17 09:27:36 -0300

    got it

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
+'use strict';
+
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
-const download = require('./download');
+const got = require('got');
 
 const app = express();
 const port = process.env.PORT || 1337;
 const folder = process.env.FOLDER || 'storage';
+
+const download = function (url, folder) {
+  return new Promise((resolve, reject) => {
+    const filename = path.basename(url);
+    const dest = path.join(folder, filename);
+    const writeStream = fs.createWriteStream(dest);
+
+    writeStream.on('finish', () => {
+      resolve(filename);
+    });
+
+    writeStream.on('error', err => {
+      fs.unlink(dest, reject.bind(null, err));
+    });
+
+    got.stream(url)
+      .pipe(writeStream);
+  });
+};
 
 app.get('/', async (req, res) => {
   try {
