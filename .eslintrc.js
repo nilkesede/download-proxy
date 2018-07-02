6e3af8f885f2bafe6930a734a5085b8297a15eb3
Nil Késede <nil@ksde.pw>
2018-07-02 09:52:03 -0300

    sign

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,9 @@ module.exports = {
   plugins: [
     'unicorn',
     'jsx-a11y'
-  ]
+  ],
+  rules: {
+    'react/jsx-closing-bracket-location': 'off',
+    'react/jsx-closing-tag-location': 'off'
+  }
 };
--- pages/index.js
+++ pages/index.js
@@ -25,34 +25,53 @@ export default class Index extends Component {
       <div>
         <Head title={title} description={description}/>
 
-        <input type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-          onKeyUp={this.download}/>
+        <div className="container">
+          <input type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            onKeyUp={this.download}
+            className="form-control"/>
+        </div>
+
+        <a href="http://ksde.pw"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="anchor">ksde.pw</a>
 
         <style global jsx>{`
-          * {
-            box-sizing: border-box;
-          }
+          * { box-sizing: border-box; }
           html, body {
             width: 100%;
             height: 100%;
             margin: 0;
             padding: 0;
+            font: 1rem 'Courier New', Courier, monospace;
+            background: black;
+          }
+          .container {
+            width: 100%;
+            height: 100vh;
             display: flex;
             justify-content: center;
             align-items: center;
-            background: black;
+            flex-direction: column;
+            margin-bottom: -18px;
           }
-          input {
+          .form-control {
             width: 70vw;
             margin: 0;
             padding: 8px 5px;
-            border: 1px solid #333;
+            border: 1px solid #fff;
             font: 1rem 'Courier New', Courier, monospace;
             background: black;
-            color: #999;
+            color: #fff;
             outline: none;
+          }
+          .anchor {
+            color: #fff;
+            text-align: center;
+            display: block;
+            text-decoration: none;
           }`}
         </style>
       </div>
